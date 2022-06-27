97c3433a22f8749ad4e292b6c6a2f7db3ab4810b
Lukasz Szawronski <lukasz.szawronski@gmail.com>
2022-06-27 20:11:03 +0200

    My new project

---
+++ src/main/java/pl/lukaszszawronski/BigPikeShop/BigPikeShopApplication.java
@@ -0,0 +1,13 @@
+package pl.lukaszszawronski.BigPikeShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigPikeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigPikeShopApplication.class, args);
+	}
+
+}
