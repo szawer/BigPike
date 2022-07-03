95d3e5390509a7a7270b42b3c2e1db220e559403
Lukasz Szawronski <lukasz.szawronski@gmail.com>
2022-07-03 12:57:54 +0200

    Added Unit Tests.

--- src/test/java/pl/lukaszszawronski/BigPikeShop/UserRepositoryTests.java
+++ src/test/java/pl/lukaszszawronski/BigPikeShop/UserRepositoryTests.java
@@ -9,6 +9,7 @@ import org.springframework.test.annotation.Rollback;
 import pl.lukaszszawronski.BigPikeShop.Entity.Role;
 import pl.lukaszszawronski.BigPikeShop.Entity.User;
 import pl.lukaszszawronski.BigPikeShop.User.UserRepository;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
@@ -23,7 +24,7 @@ public class UserRepositoryTests {
     private TestEntityManager entityManager;
 
     @Test
-    public void testCreateUser(){
+    public void testCreateNewUserWithOneRole(){
         Role roleAdmin = entityManager.find(Role.class, 1);
         User userLukaS = new User("lukasz.szawronski@gmail.com",
                 "luka2022",
@@ -34,5 +35,54 @@ public class UserRepositoryTests {
         User savedUser = repo.save(userLukaS);
 
         assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+    @Test
+    public void testCreateNewUserWithTwoRoles() {
+        User userLuka1 = new User("szawercq@interia.pl",
+                "luka12022",
+                "Luka",
+                "Szawr");
+        Role roleEditor = new Role(3);
+        Role roleAssistant = new Role(5);
+        userLuka1.addRole(roleEditor);
+        userLuka1.addRole(roleAssistant);
+
+        User savedUser = repo.save(userLuka1);
+
+        assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testListAllUsers(){
+        Iterable<User> listUsers = repo.findAll();
+        listUsers.forEach(user -> System.out.println(user));
+    }
+    @Test
+    public void testGetUserByID(){
+        User userLukaS = repo.findById(1).get();
+        System.out.println(userLukaS);
+        assertThat(userLukaS).isNotNull();
+    }
+    @Test
+    public void testUpdateUserDetails(){
+        User userLukaS = repo.findById(1).get();
+        userLukaS.setEnabled(true);
+        userLukaS.setEmail("szawero@gmail.com");
+
+        repo.save(userLukaS);
+    }
+    @Test
+    public void testUpdateUserRoles(){
+        User userLuka1 = repo.findById(2).get();
+        Role roleEditor = new Role(3);
+        Role roleSalesperson = new Role(2);
+        userLuka1.getRoles().remove(roleEditor);
+        userLuka1.addRole(roleSalesperson);
+        repo.save(userLuka1);
+    }
+    @Test
+    public void testDeleteUser(){
+        Integer userId = 2;
+        repo.deleteById(userId);
     }
 }
