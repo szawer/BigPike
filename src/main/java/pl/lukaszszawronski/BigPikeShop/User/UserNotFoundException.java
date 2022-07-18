3feb43a18616cdce4f9461409248fc6fdcc924bd
Lukasz Szawronski <lukasz.szawronski@gmail.com>
2022-07-18 20:58:24 +0200

    Updated: User functions

---
+++ src/main/java/pl/lukaszszawronski/BigPikeShop/User/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package pl.lukaszszawronski.BigPikeShop.User;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/pl/lukaszszawronski/BigPikeShop/User/UserService.java
+++ src/main/java/pl/lukaszszawronski/BigPikeShop/User/UserService.java
@@ -7,6 +7,7 @@ import pl.lukaszszawronski.BigPikeShop.Entity.Role;
 import pl.lukaszszawronski.BigPikeShop.Entity.User;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class UserService {
@@ -27,15 +28,49 @@ public class UserService {
     }
 
     public void save(User user) {
-        encodePassword(user);
+        boolean isUpdatingUser = user.getId() != null;
+
+        if (isUpdatingUser){
+            User existingUser = userRepo.findById(user.getId()).get();
+
+            if (user.getPassword().isEmpty()){
+                user.setPassword(existingUser.getPassword());
+            } else {
+                encodePassword(user);
+            }
+
+        } else {
+            encodePassword(user);
+        }
         userRepo.save(user);
     }
     private void encodePassword(User user){
         String encodedPassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(encodedPassword);
     }
-    public boolean isEmailUnique(String email){
+    public boolean isEmailUnique(Integer id, String email){
         User userByEmail = userRepo.getUserByEmail(email);
+
+        if (userByEmail == null) return  true;
+        boolean isCreatingNew = (id == null);
+
+        if (isCreatingNew) {
+            if (userByEmail != null) return false;
+        } else {
+            if (userByEmail.getId() != id) {
+                return false;
+            }
+            return true;
+        }
+
         return  userByEmail == null;
+    }
+
+    public User get(Integer id) throws UserNotFoundException {
+        try {
+            return userRepo.findById(id).get();
+        } catch (NoSuchElementException ex){
+            throw new UserNotFoundException("Could not find any user with ID: " + id);
+        }
     }
 }
