af9f069fb3de0900bd73309abb62ed5e9db6a5a6
Lukasz Szawronski <lukasz.szawronski@gmail.com>
2022-07-28 20:42:42 +0200

    Added: User photo upload function

--- src/main/java/pl/lukaszszawronski/BigPikeShop/User/UserController.java
+++ src/main/java/pl/lukaszszawronski/BigPikeShop/User/UserController.java
@@ -3,13 +3,18 @@ package pl.lukaszszawronski.BigPikeShop.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import pl.lukaszszawronski.BigPikeShop.Admin.FileUploadUtil;
 import pl.lukaszszawronski.BigPikeShop.Entity.Role;
 import pl.lukaszszawronski.BigPikeShop.Entity.User;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -35,9 +40,17 @@ public class UserController {
         return "user_form";
     }
     @PostMapping("/users/save")
-    public String saveUser(User user, RedirectAttributes redirectAttributes){
-        System.out.println(user);
-        service.save(user);
+    public String saveUser(User user, RedirectAttributes redirectAttributes,
+                           @RequestParam("image")MultipartFile multipartFile) throws IOException {
+        if (multipartFile.isEmpty()){
+            String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+            user.setPhotos(fileName);
+            User savedUser = service.save(user);
+            String uploadDir = "user-photos/" + savedUser.getId();
+            FileUploadUtil.saveFile(uploadDir,fileName,multipartFile);
+        }
+
+//        service.save(user);
         redirectAttributes.addFlashAttribute("message", "The user has been saved successfully!");
         return "redirect:/users";
     }
